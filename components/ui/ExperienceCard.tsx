edb63c120eed0530dab26f156c4f6280f31acd29
rizwanwebdev <rizwanzia025@gmail.com>
2026-02-10 15:15:05 +0500

    cards coorected

--- components/ui/ExperienceCard.tsx
+++ components/ui/ExperienceCard.tsx
@@ -7,12 +7,12 @@ export type Card = {
 export function ExperienceCard({ title, type, desc, side }: Card) {
   return (
     <div
-      className={`bg-primary relative rounded-lg py-4 px-5 text-background flex flex-col gap-2 timeline-item ${side === "left" ? "left" : "right"}`}
+      className={`bg-primary relative tracking-wide rounded-lg p-6 flex flex-col gap-4 timeline-item ${side === "left" ? "left" : "right"}`}
     >
       <span className="dot"></span>
-      <h3 className="text-4xl leading-11">{title}</h3>
-      <p className="text-xl tracking-wide">{type}</p>
-      <p className="tracking-wide">{desc}</p>
+      <h3 className="text-4xl text-background  leading-11">{title}</h3>
+      <p className="text-xl text-background/90  tracking-wide">{type}</p>
+      <p className="tracking-wider font-normal text-background ">{desc}</p>
     </div>
   );
 }
